df1c382a1d3e95247618ba2bdceaeb932f01385f
Nithin <nithinsurendran321@gmail.com>
2021-12-04 02:24:55 +0530

    admin nav and toolbar component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterceptorService } from './utilities/interceptor/interceptor.service';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './admin/auth/login/login.component';
+import { AdminNavbarComponent } from './admin/partials/admin-navbar/admin-navbar.component';
+import { AdminToolbarComponent } from './admin/partials/admin-toolbar/admin-toolbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LoginComponent,
+    AdminNavbarComponent,
+    AdminToolbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide:HTTP_INTERCEPTORS,useClass:InterceptorService,multi:true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
