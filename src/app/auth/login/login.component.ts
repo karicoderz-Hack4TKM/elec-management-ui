c2eafb87137988ec81f241250f151ef1372596ec
Razik <rasikmhd10@gmail.com>
2021-12-04 22:22:10 +0530

    login added admin sub station and user

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,24 +9,50 @@ import { AuthService } from '../auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  public loading =false;
-  constructor(private authService:AuthService) { }
+  public loading = false;
+  public error: boolean = false
+  admin = {
+    email:"elec@gmail.com",
+    password:'password'
+  }
+  substation = {
+    email:"SUB001@gmail.com",
+    password:'password'
+  }
+  constructor(private authService: AuthService,private router:Router) { }
 
   ngOnInit(): void {
   }
-  login(form:NgForm){
-    this.loading=true
+  login(form: NgForm) {
+    this.loading = true
     let data = {
-      email:form.value.email.trim(),
-      password:form.value.password.trim()
+      email: form.value.email.trim(),
+      password: form.value.password.trim()
     }
-    this.authService.getUser(data).subscribe(
-      res => {
-        
-      }
-    )
+    if(data.email == this.admin.email && data.password == this.admin.password){
+      this.router.navigate(['/admin']);
+    }else if (data.email == this.substation.email && data.password == this.substation.password){
+      this.router.navigate(['/substation']);
+    }else{
+      this.authService.getUser(data).subscribe(
+        res => {
+          console.log(res)
+          if (res.code == 200) {
+            localStorage.setItem('token',res.data.token)
+              this.router.navigate(['/user']);
+              this.loading = false
+          }else{
+            this.error = true
+            this.loading = false
+            form.resetForm()
+          }
+        }
+      )
+    }
 
     
+
+
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  API:'http://localhost:9900/',
+  API:'http://localhost:5000/',
   production: false
 };
 
