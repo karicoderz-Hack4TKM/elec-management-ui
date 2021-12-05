53512182599a8891371cc161e5153695f59c79e6
Razik <rasikmhd10@gmail.com>
2021-12-05 08:02:04 +0530

    regular update

--- src/app/substation/substation-users/substation-users.component.ts
+++ src/app/substation/substation-users/substation-users.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-substation-users',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./substation-users.component.css']
 })
 export class SubstationUsersComponent implements OnInit {
-
-  constructor() { }
+  users:any = []
+  constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
+    this.loadUsers()
+  }
+  loadUsers(){
+    return this.http.get<any>(environment.API+'core/users').subscribe(
+      res => {
+        console.log(res)
+        this.users = res.data
+      }
+    )
   }
 
 }
